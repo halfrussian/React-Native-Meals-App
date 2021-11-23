a376a3a0896fb1f8956960755a89fba2af596d01
Aaron Bracken <joshmidla@gmail.com>
2021-11-22 20:37:57 -0600

    literally forgot to commit for like a week, get it together bro

--- App.js
+++ App.js
@@ -4,33 +4,54 @@ import React from 'react';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restuarants.screen';
 import { ThemeProvider } from 'styled-components/native';
 import {theme} from './src/infrastructure/theme'
-
-
 import {useFonts as useOswald ,Oswald_400Regular}  from '@expo-google-fonts/oswald'
 import {useFonts as useLato,Lato_400Regular}  from '@expo-google-fonts/lato'
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeArea } from './src/components/utils/safe-area.component';
+import {Ionicons} from '@expo/vector-icons';
+
+
+
+const Settings = () => (
+  
+<SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
-
   const [oswaldLoaded] = useOswald({
-    Oswald_400Regular
-  });
-  const [latoLoaded] = useLato({
-    Lato_400Regular
+    Oswald_400Regular,
   });
 
-  if(!oswaldLoaded || !latoLoaded ) {
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
-
-
+  const Tab = createBottomTabNavigator();
 
   return (
     <>
-    <ThemeProvider theme={theme}>
-    <RestaurantsScreen />
-    </ThemeProvider>
-    <ExpoStatusBar style='auto' />
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="Map" component={Map} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
--- src/features/restaurants/screens/restuarants.screen.js
+++ src/features/restaurants/screens/restuarants.screen.js
@@ -1,41 +1,50 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Searchbar } from "react-native-paper";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 
-const SafeArea = styled(SafeAreaView)`
- flex: 1;
- ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
-`
-
+import { SafeArea } from "../../../components/utils/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SearchContainer = styled.View`
-padding: ${(props) => props.theme.space[3]};
-`
+  padding: ${(props) => props.theme.space[3]};
+`;
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
-const RestaurantListContainer = styled.View`
-    flex: 1;
-    padding: ${(props) => props.theme.space[3]};
-  
-`
-
-
-export const RestaurantsScreen = ()=> {
-
-
-  return (
+export const RestaurantsScreen = () => (
   <SafeArea>
-      <SearchContainer>
+    <SearchContainer>
       <Searchbar />
-      </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
-    </SafeArea>
-
-  )
-}
-
+    </SearchContainer>
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  </SafeArea>
+);
